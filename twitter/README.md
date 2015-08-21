db15eab3f74e177dda5aa6a5dd59a57c243c27bc
Michael Barrett <twisterghost@gmail.com>
2015-08-21 02:20:04 +0000

    Prepare twitter module for npm

--- twitter/README.md
+++ twitter/README.md
@@ -1,18 +1,27 @@
-#Twitter module for Jankbot
+# Twitter module for Jankbot
 By JankDota
-
-To use this module, copy this directory into Jankbot's `bot_modules/` directory.
 
 ## Setup
 
-After copying this module into your `bot_modules/` directory, run `npm install twit` to install
-dependencies for this module.
-
 This module requires a twitter API key. You will need to generate a twitter API
 key by logging in to [twitter's dev site](http://dev.twitter.com) and making
-a new application. Paste your consumer and access tokens into `tweet.json`, and
-put whatever steam IDs you want to allow to tweet in the `allowed` array in
-the `tweet.json` file.
+a new application.
+
+Next, make a file in `data/` in your Jankbot directory called `tweet.json` and use this template:
+
+```javascript
+{
+  "consumer_key":         "YOUR-CONSUMER-KEY",
+  "consumer_secret":      "YOUR-CONSUMER-SECRET",
+  "access_token":         "YOUR-ACCESS-TOKEN",
+  "access_token_secret":  "YOUR-ACCESS-TOKEN-SECRET",
+  "allowed": [
+    "YOUR-STEAM-ID"
+  ]
+}
+```
+
+Fill out the consumer and access tokens, and put the list of approved user IDs in the `allowed` array.
 
 ## Commands
 
--- twitter/twitter.js
+++ twitter/twitter.js
@@ -1,29 +1,36 @@
 var Twit = require('twit');
 var fs = require("fs");
 
-exports.compatible = '2.0.*';
-
-var config = require('./tweet.json');
-
-var T = new Twit({
-    consumer_key:         config.consumer_key
-  , consumer_secret:      config.consumer_secret
-  , access_token:         config.access_token
-  , access_token_secret:  config.access_token_secret
-});
+var config;
+var T;
+if (fs.existsSync("./data/tweet.json")) {
+  config = JSON.parse(fs.readFileSync("./data/tweet.json"));
+  T = new Twit({
+      consumer_key:         config.consumer_key
+    , consumer_secret:      config.consumer_secret
+    , access_token:         config.access_token
+    , access_token_secret:  config.access_token_secret
+  });
+}
 
 // Import Jankbot API modules.
 var friends = require("../../core/friends");
 var logger = require('../../core/logger.js');
 
 exports.handle = function(input, source) {
+
   var tweetConfig = config;
   input = input.split(" ");
   if (input[0] == "tweet") {
 
+    if (!tweetConfig) {
+      friends.messageUser(source, "The twitter module is missing data/tweet.json. Please see the README");
+      return true;
+    }
+
     if (tweetConfig.allowed.indexOf(source) == -1) {
       friends.messageUser(source, "You do not have the permissions to use this module.");
-      return;
+      return true;
     }
 
     // Parse the input and decide what to do.
@@ -31,7 +38,7 @@ exports.handle = function(input, source) {
     var content = input.join(" ");
 
     if (content.length > 140) {
-      friends.messageUser(source, "Toooo long! (Can you feeeel iiiiit?)");
+      friends.messageUser(source, "Message was >140 characters. Not tweeted.");
       return;
     } else {
       T.post('statuses/update', {status: content}, function(err, reply) {
